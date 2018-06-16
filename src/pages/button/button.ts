9696ca44da595906b0498425a2f117262f20d73b
Federico Amura <federico.amura@viapool.com>
2018-06-16 16:26:49 -0300

    mock de pagina de botones

--- src/pages/button/button.ts
+++ src/pages/button/button.ts
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class ButtonPage {
 
+  buttons = [
+    {id: 1, name: "Boton 1", dsn: "G030 MD04 9341 4C4M", trigger: 1},
+    {id: 2, name: "Boton 2", dsn: "G030 MD67 5844 73JR", trigger: null}
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  public addButton() {
+    console.log("apretamos el boton");
   }
 
 }
