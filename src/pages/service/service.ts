1cd541cc414ca9dbe8e0eac4c3826747444edd3f
Federico Amura <federico.amura@viapool.com>
2018-06-17 23:44:06 -0300

    compra del noble repulgue mockeada

--- src/pages/service/service.ts
+++ src/pages/service/service.ts
@@ -14,7 +14,9 @@ export class ServicePage {
         {name: "Alimento del perro", description: "Dog chow", price: 580}
       ]},
     {icon: "jumbo.png", name: "Jumbo", stars: 4.2, telephone: "0800-436-6452", orders: []},
-    {icon: "nobleRepulgue.png", name: "Noble repulgue", stars: 3.7, telephone: "0800-756-2524", orders: []}
+    {icon: "nobleRepulgue.png", name: "Noble repulgue", stars: 3.7, telephone: "0800-756-2524", orders: [
+        {name: "Docena simple", description: "6 carne y 6 JQ", price: 360}
+      ]}
   ];
 
   constructor(public navCtrl: NavController) {
