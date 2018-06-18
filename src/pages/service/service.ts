8326125b76fd5d497609dc44838208298f4c6a9a
Federico Amura <federico.amura@viapool.com>
2018-06-17 23:29:18 -0300

    compras del servicio mockeadas

--- src/pages/service/service.ts
+++ src/pages/service/service.ts
@@ -9,7 +9,10 @@ import { IonicPage, NavController } from 'ionic-angular';
 export class ServicePage {
 
   services = [
-    {icon: "coto.png", name: "Coto", stars: 4.4, telephone: "0800-222-1234", orders: []},
+    {icon: "coto.png", name: "Coto", stars: 4.4, telephone: "0800-222-1234", orders: [
+        {name: "Compra semanal", description: "Insumos basicos", price: 950},
+        {name: "Alimento del perro", description: "Dog chow", price: 580}
+      ]},
     {icon: "jumbo.png", name: "Jumbo", stars: 4.2, telephone: "0800-436-6452", orders: []},
     {icon: "nobleRepulgue.png", name: "Noble repulgue", stars: 3.7, telephone: "0800-756-2524", orders: []}
   ];
