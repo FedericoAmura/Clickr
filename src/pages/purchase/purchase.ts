0e6f57c21048710a1c80e987e9557bfbe1d42f93
Federico Amura <federico.amura@viapool.com>
2018-06-18 17:04:00 -0300

    pagina de compras mockeada

--- src/pages/purchase/purchase.ts
+++ src/pages/purchase/purchase.ts
@@ -8,6 +8,11 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class PurchasePage {
 
+  purchases = [
+    {id: 1, name: "Coto - Compra semanal", status: "Entregado", cancellable: false, when: "2018/06/11 18:35:25"},
+    {id: 2, name: "Coto - Compra semanal", status: "En camino", cancellable: true, when: "Hace 2 horas"}
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
