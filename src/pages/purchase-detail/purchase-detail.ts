af12d0babab7e005bfcb8ed5b2d80c6262405f5f
Federico Amura <federico.amura@viapool.com>
2018-06-20 17:12:11 -0300

    agregado productos y sacado boton de cancelacion en detalles de las compras hechas

--- src/pages/purchase-detail/purchase-detail.ts
+++ src/pages/purchase-detail/purchase-detail.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { Purchase, PurchaseProvider} from "../../providers/purchase/purchase";
-import { Service, ServiceProvider} from "../../providers/service/service";
+import { Purchase, PurchaseProvider } from "../../providers/purchase/purchase";
+import { Service, ServiceProvider } from "../../providers/service/service";
+import { Pack } from "../../providers/order/order";
+import { Product, ProductProvider } from "../../providers/product/product";
 
 @IonicPage()
 @Component({
@@ -11,12 +13,18 @@ import { Service, ServiceProvider} from "../../providers/service/service";
 })
 export class PurchaseDetailPage {
 
-  purchase: Purchase;
   service: Service;
+  purchase: Purchase;
+  products: (Product&Pack)[];
+  cost: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public purchaseProvider: PurchaseProvider, public serviceProvider: ServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public productProvider: ProductProvider, public purchaseProvider: PurchaseProvider, public serviceProvider: ServiceProvider) {
     this.purchase = this.purchaseProvider.getPurchase(this.navParams.get('purchase'));
     this.service = this.serviceProvider.getService(this.purchase.serviceId);
+    this.products = this.purchase.products.map(pack => {
+      return {...pack,...this.productProvider.getProduct(pack.productId)};
+    });
+    this.cost = this.products.reduce((sub, product) => sub+product.amount*product.price, 0);
   }
 
 }
--- src/providers/purchase/purchase.ts
+++ src/providers/purchase/purchase.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@angular/core';
 
+import { Pack } from "../order/order";
+
 @Injectable()
 export class PurchaseProvider {
 
   purchases: Purchase[] = [
-    {id: 1, name: "Coto - Compra semanal", serviceId: 1, status: "Entregado", cancellable: false, when: "2018/06/11 18:35:25", delivery: "2018/06/13 19:22:45", from: "Sucursal Cabildo"},
-    {id: 2, name: "Coto - Compra semanal", serviceId: 1, status: "En camino", cancellable: true, when: "Hace 2 horas", delivery: null, from: "Sucursal Cabildo"}
+    {
+      id: 1,
+      name: "Coto - Compra semanal",
+      serviceId: 1,
+      products: [
+        {productId: 1, amount: 3},
+        {productId: 2, amount: 1},
+        {productId: 3, amount: 1}],
+      status: "En camino",
+      cancellable: true,
+      when: "Hace 2 horas",
+      delivery: null,
+      from: "Sucursal Cabildo"
+    },
+    {
+      id: 2,
+      name: "Coto - Compra semanal",
+      serviceId: 1,
+      products: [
+        {productId: 1, amount: 2},
+        {productId: 2, amount: 1}],
+      status: "Entregado",
+      cancellable: false,
+      when: "2018/06/11 18:35:25",
+      delivery: "2018/06/13 19:22:45",
+      from: "Sucursal Cabildo"
+    }
   ];
 
   constructor() {
@@ -26,6 +53,7 @@ export class Purchase {
   id: number;
   name: string;
   serviceId: number;
+  products: Pack[];
   status: string;
   cancellable: boolean;
   when: string;
